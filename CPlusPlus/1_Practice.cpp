e9a2a53369caad09211f11ca070068d50504b403
DVA1145@outlook.com <DVA1145@outlook.com>
2024-04-18 23:02:33 +0300

    Add project files.

---
+++ CPlusPlus/1_Practice.cpp
@@ -0,0 +1,54 @@
+//#include <iomanip>
+//#include <cmath>
+//#include <iostream>
+//using namespace std;
+
+//void main()
+//{
+	/*
+	int n;
+	cout << "n : "; cin >> n;
+	double y;
+	y = (((cbrt(n - 1) * (double)log(pow(2, n))) - ((double)tan((n + 1) / pow(n, 3)))) / ((double)acos(0.85 + log(n)))) + 2.75;
+	cout << "resultat : " << y << endl;
+	*/
+
+
+
+
+	/*
+	float x = 0.6, s = 5.6;
+	double r, r1;
+	r = sin(x) + log(s);
+	r1 = sqrt(pow(r, 3));
+	cout << "x : " << x << endl;
+	cout << "s : " << s << endl;
+	cout << "r : " << r << endl;
+	cout << "r1 : " << r1 << endl;
+	*/
+
+
+
+	
+	/*
+	const int size = 6;
+	int matrix[size];
+	int min_chislo;
+	int suma_chisel = 0;
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Enter " << i + 1 << " number : "; cin >> matrix[i];
+	}
+	min_chislo = matrix[0];
+	cout << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cout << matrix[i] << ' ';
+		suma_chisel += matrix[i];
+		if (matrix[i] < min_chislo) min_chislo = matrix[i];
+	}
+	cout << endl;
+	cout << "Minimalne chislo : " << min_chislo << endl;
+	cout << "Suma chisel : " << suma_chisel << endl;
+	*/
+//}
---
+++ CPlusPlus/2_Practice.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+enum Month { June = 1, July, August };
+
+void main()
+{
+	/*
+	bool isLezhut = false;
+	float x, y;
+	cout << "x : "; cin >> x;
+	cout << "y : "; cin >> y;
+	if (y >= 1 && x < 0 && x > -(y - 1)) isLezhut = true;
+	if (-x + y <= 1 && -x > 0 && y > 0) isLezhut = true;
+	if (-x >= 1 && y < 0 && y > -(x + 1)) isLezhut = true;
+	if (x > 0 && y < 0 && x < -(y - 1)) isLezhut = true;
+	if (x > 0 && y > 0 && x + y > 1 && y < x + 1) isLezhut = true;
+	if (isLezhut == true) cout << "Lezhut" << endl;
+	else cout << "Ne lezhut" << endl;
+	*/
+
+
+
+
+	/*
+	int a;
+	cout << "number : "; cin >> a;
+	if (a % 2 == 0) cout << "Parne" << endl;
+	else cout << "Ne parne";
+	*/
+
+
+
+
+	double a, june, july, august;
+	cout << "price kupalnika : "; cin >> a;
+	double p;
+	cout << "p% : "; cin >> p;
+	double q;
+	cout << "q% : "; cin >> q;
+	cout << "May : " << a << endl;
+	june = a + (a / 100 * p);
+	cout << "June : " << june << endl;
+	july = a;
+	cout << "July : " << july << endl;
+	august = a - (a / 100 * q);
+	cout << "August : " << august << endl;
+	cout << "=================" << endl;
+	int choice = 0;
+	bool isValidData = true;
+	while (choice < 1 || choice > 3)
+	{
+		if (!isValidData) cout << "Error data. Try again!" << endl;
+		isValidData = false;
+		cout << "1 - June" << endl;
+		cout << "2 - July" << endl;
+		cout << "3 - August" << endl;
+		cout << "Enter choice : "; cin >> choice;
+	}
+	switch (choice)
+	{
+	case Month::June: cout << "June price : " << june << endl;
+	case Month::July: cout << "July price : " << july << endl;
+	case Month::August: cout << "August price : " << august << endl;
+	}
+}
