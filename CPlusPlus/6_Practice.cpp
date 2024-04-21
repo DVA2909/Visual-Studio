da305042f45cdf2c6afffa145bc6d40ebb0fd346
DVA1145@outlook.com <DVA1145@outlook.com>
2024-04-21 20:49:53 +0300

    6_Practice

---
+++ CPlusPlus/6_Practice.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+
+int main()
+{
+	/*
+	int r = 6;
+	int c = 5;
+	int** votes = new int* [r];
+	for (int i = 0; i < r; i++)
+	{
+		*(votes + i) = new int[c];
+	}
+	
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			votes[i][j] = (i + 1) * (j + 1);
+		}
+	}
+
+	cout << "Table of voting results : " << endl;
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			cout << setw(2) << votes[i][j] << ' ';
+		}
+		cout << endl;
+	}
+	cout << endl;
+
+	cout << "Number of voters in each locality : " << endl;
+	for (int i = 0; i < r; i++)
+	{
+		int voters = 0;
+		for (int j = 0; j < c; j++)
+		{
+			voters += votes[i][j];
+		}
+		cout << "Settlement " << (i + 1) << " : " << voters << "voters" << endl;
+	}
+
+
+
+	for (int i = 0; i < r; i++)
+	{
+		delete[] * (votes + i);
+	}
+	delete[] votes;
+	*/
+
+}
