46cbf99cad1d94ba2e4155b10a74ab94730aac6c
DVA1145@outlook.com <DVA1145@outlook.com>
2024-04-21 21:16:41 +0300

    6_Practice

--- CPlusPlus/6_Practice.cpp
+++ CPlusPlus/6_Practice.cpp
@@ -54,4 +54,52 @@ int main()
 	delete[] votes;
 	*/
 
+
+
+
+	/*
+	srand(time(NULL));
+	int size = 6;
+	int** matrix = new int* [size];
+	for (int i = 0; i < size; i++)
+	{
+		*(matrix + i) = new int[size];
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			matrix[i][j] = rand() % 26 - 5;
+		}
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			cout << setw(2) << matrix[i][j] << ' ';
+		}
+		cout << endl;
+	}
+
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if ((i == 0 || j == 0) || (i == size - 1 || j == size - 1))
+				sum += matrix[i][j];
+		}
+	}
+	cout << "Summa po perimetru : " << sum << endl;
+
+
+
+	for (int i = 0; i < size; i++)
+	{
+		delete[] * (matrix + i);
+	}
+	delete[] matrix;
+	*/
 }
