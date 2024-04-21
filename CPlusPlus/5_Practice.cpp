6eeea273b910b7f28aef069f687c1c0494b2ed2d
DVA1145@outlook.com <DVA1145@outlook.com>
2024-04-21 19:54:48 +0300

    5_Practice

--- CPlusPlus/5_Practice.cpp
+++ CPlusPlus/5_Practice.cpp
@@ -1,23 +1,36 @@
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
+// 
+//void Max(int*& matrix, int& max_index, int size)
+//{
+//	int max_number = 0;
+//	for (int i = 0; i < size; i++)
+//	{
+//		if (*(matrix + i) > max_number && *(matrix + i) % 2 != 0)
+//		{
+//			max_number = *(matrix + i);
+//			max_index = i;
+//		}
+//	}
+//}
 
-void Max(int*& matrix, int& max_index, int size)
-{
-	int max_number = *matrix;
-	for (int i = 0; i < size; i++)
-	{
-		if (*(matrix + i) > max_number && *(matrix + i) % 2 == 0)
-		{
-			max_number = *(matrix + i);
-			max_index = i;
-		}
-	}
-}
+
+//int Min(int* matrix, int size)
+//{
+//	int min_number = *matrix;
+//	for (int i = 0; i < size; i++)
+//	{
+//		if (*(matrix + i) < 0) break;
+//		if (*(matrix + i) < min_number) min_number = *(matrix + i);
+//	}
+//	return min_number;
+//}
 
 
 
-void main()
-{
+//void main()
+//{
+	/*
 	srand(time(NULL));
 	int size;
 	cout << "matrix size : ";
@@ -30,4 +43,31 @@ void main()
 	Max(matrix, max_index, size);
 	cout << "Max even number in matrix : " << *(matrix + max_index) << endl;
 	delete[] matrix;
-}
+	*/
+
+
+
+
+	/*
+	srand(time(NULL));
+	int size = 12;
+	int* height = new int[size];
+	for (int i = 0; i < size; i++) *(height + i) = rand() % 28 + 163;
+	for (int i = 0; i < size; i++) cout << *(height + i) << ' ';
+	cout << endl;
+	*/
+
+
+
+
+	/*
+	srand(time(NULL));
+	int size = 20;
+	int* matrix = new int[size];
+	for (int i = 0; i < size; i++) *(matrix + i) = rand() % 41 - 10;
+	for (int i = 0; i < size; i++) cout << *(matrix + i) << ' ';
+	cout << endl;
+	int res(Min(matrix, size));
+	cout << "Min element : " << res << endl;
+	*/
+//}
