3f95015bda61af8c19e694c70266d9799a5fe411
DVA1145@outlook.com <DVA1145@outlook.com>
2024-05-14 23:25:07 +0300

    4

--- CPlusPlus/4_Practice.cpp
+++ CPlusPlus/4_Practice.cpp
@@ -1,35 +1,35 @@
-//#include <iostream>
-//#include <cmath>
-//using namespace std;
-//
-//template<typename T>
-//void Min(T a, T b)
-//{
-//	a < b ? cout << a << " < " << b << endl : cout << a << " > " << b << endl;
-//}
-//
-//int Func(int a, int b)
-//{
-//	int k = 1;
-//	for (int i = a; i <= b; i++) k *= a;
-//	return k;
-//}
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+template<typename T>
+void Min(T a, T b)
+{
+	a < b ? cout << a << " < " << b << endl : cout << a << " > " << b << endl;
+}
+
+int Func(int a, int b)
+{
+	int k = 1;
+	for (int i = a; i <= b; i++) k *= a;
+	return k;
+}
 
 
 
-//void main()
-//{
-	/*
-	double a, b;
+void main()
+{
+	
+	/*double a, b;
 	cout << "First number : "; cin >> a;
 	cout << "Second number : "; cin >> b;
-	Min(a, b);
-	*/
+	Min(a, b);*/
+	
 
 
 
 
-	/*
+	
 	double G;
 	float ak = 0.8, bk = -0.4;
 	const double x = -4.3, z = 2.8;
@@ -37,5 +37,5 @@
 	G += (bk - pow(sin(ak), 2)) / (log(3) * -(1.3 - x * z));
 	G -= (log(Func(3, 7)) * -(z - 1.3 * x)) / (ak - pow(sin(ak), 3));
 	cout << "G : " << G << endl;
-	*/
-//}
+	
+}
