c955c55522e150d78872e57d861b336312bf9c37
DVA1145@outlook.com <DVA1145@outlook.com>
2024-05-14 23:02:07 +0300

    3

--- CPlusPlus/3_Practice.cpp
+++ CPlusPlus/3_Practice.cpp
@@ -1,26 +1,26 @@
-//#include <iostream>
-//using namespace std;
+#include <iostream>
+using namespace std;
 
 
 
-//void main()
-//{
-	/*
-	float kmInDay = 10;
+void main()
+{
+	
+	/*float kmInDay = 10;
 	int days = 0;
 	do
 	{
 		kmInDay += kmInDay / 100 * 10;
 		days++;
 	} while (kmInDay <= 20);
-	cout << "Cherez " << days << " dniv norma bude bilshe 20" << endl;
-	*/
+	cout << "Cherez " << days << " dniv norma bude bilshe 20" << endl;*/
+	
 
 
 
 
-	/*
-	double Vmax, Vcar;
+	
+	/*double Vmax, Vcar;
 	cout << "Vmax : "; cin >> Vmax;
 	int count = 0;
 	do
@@ -29,17 +29,17 @@
 		cout << "V" << count << " : "; cin >> Vcar;
 		if (Vmax < Vcar) cout << "!!!! PORUSHNYK !!!!" << endl;
 		cout << "------------" << endl;
-	} while (true);
-	*/
+	} while (true);*/
+	
 
 
 
 
-	/*
-	float S, m;
+	
+	/*float S, m;
 	cout << "m : ";
 	cin >> m;
 	S = m * 1;
-	cout << "m = " << S << endl;
-	*/
-//}
+	cout << "m = " << S << endl;*/
+	
+}
