e574f8d603695cfcc2bcff9ad7c8938021c7782d
DVA1145@outlook.com <DVA1145@outlook.com>
2024-05-15 22:19:06 +0300

    11

--- Python/11_Practice.py
+++ Python/11_Practice.py
@@ -1,1 +1,38 @@
+import math
 
+
+# Завдання 1
+# a = 2.0  # Assuming a = 2.0
+
+# k = float(input("Enter the value of k: "))
+
+# if k < 2 * a:
+#     f_k = k + b / (2 + math.sin(a))
+# elif 2 * a <= k < 2 * a + 1:
+#     f_k = k + math.sin(k ** 2)
+# else:  # k >= 2 * a + 1
+#     f_k = math.sqrt(a + 4 * math.sin(a + b))
+
+# print(f"Input value of k: {k}")
+# print(f"Value of a: {a}")
+# print(f"Value of f(k): {f_k:.4f}")
+
+
+
+
+
+# Завдання 2
+# SQRT_2 = math.sqrt(2)  # Square root of 2
+
+# radius = float(input("Enter the radius of the circle: "))
+# square_side = float(input("Enter the length of the square side: "))
+
+# can_pass = radius >= square_side / (2 * SQRT_2)
+
+# print(f"Radius of the circle: {radius:.2f}")
+# print(f"Length of the square side: {square_side:.2f}")
+
+# if can_pass:
+#     print("The circle can pass through the square.")
+# else:
+#     print("The circle cannot pass through the square.")
